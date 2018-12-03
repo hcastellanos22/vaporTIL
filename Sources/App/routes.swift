0b020c53e07c136f0f0b8e59ef64d5c8d4760c28
Hernan Castellanos Palencia <hernancastellanospalencia@Hernans-MacBook-Pro.local>
2018-12-02 19:16:01 -0600

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,10 +11,26 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    // 1 Register a new route at /api/acronyms that accepts a POST request and returns Future<Acronym>.
+    
+    router.post("api", "acronyms") { req -> Future<Acronym> in
+        
+        // 2 Decode JSON request into an acronym model.
+        //This returns Future<Acronym>. flatMap to extract the acronym
+        return try req.content.decode(Acronym.self)
+            .flatMap(to: Acronym.self) { acronym in
+                
+                // 3 Save model using Fluent
+                return acronym.save(on: req)
+        }
+    }
+    
+    //Route
 
-    // Example of configuring a controller
+    /* Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.delete("todos", Todo.parameter, use: todoController.delete) */
 }
